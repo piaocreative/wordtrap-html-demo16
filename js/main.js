057dfc079d7c801f1ee0fb8e9f80a4d94e2fe8fe
piaocreative <johnpiao171019@gmail.com>
2021-08-19 19:45:12 +0800

    demo16-0819

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$( document ).ready( function() {
+    
+    if ( $( '.logos' ).length ) {
+        $( '.logos' ).slick({
+            infinite: true,
+            slidesToShow: 6,
+            slidesToScroll: 6,
+            dots: false,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                }
+            ]
+        });
+    } 
+
+    $( '.testimonials' ).slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        arrows:true
+    });
+
+} );
