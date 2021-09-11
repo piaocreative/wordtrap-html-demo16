fc627f42d6f381542d17d2fbbd3948695889c93a
piaocreative <johnpiao171019@gmail.com>
2021-09-11 21:55:41 +0800

    demo16-0911

--- js/main.js
+++ js/main.js
@@ -20,40 +20,44 @@ $( document ).ready( function() {
                     breakpoint: 768,
                     settings: {
                         infinite: true,
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
                     }
                 },
                 {
                     breakpoint: 480,
                     settings: {
                         infinite: true,
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
                     }
                 }
             ]
         });
     } 
 
-    $( '.testimonials' ).slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-        arrows:true
-    });
-
-    $( '.collections' ).slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-        arrows:false
-    });
+    if ( $( '.testimonials' ).length ) {
+        $( '.testimonials' ).slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            adaptiveHeight: true,
+            arrows:true
+        });
+    } 
+    
+    if ( $( '.collections' ).length ) {
+        $( '.collections' ).slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            adaptiveHeight: true,
+            arrows:false
+        });
+    }
 
 } );
